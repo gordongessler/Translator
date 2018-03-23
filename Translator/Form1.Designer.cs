351e55b632ef98af70a3ea5616ced1247171d75b
Maciej Sobczynski <gryfon1997@gmail.com>
2018-03-23 14:04:54 +0100

    All funcionality of "File upload" implemented.

--- Translator/Form1.Designer.cs
+++ Translator/Form1.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.column1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.column2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.RichTextBox();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.translateButton = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.menuStrip1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -58,23 +58,25 @@
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.loadToolStripMenuItem,
-            this.exportToolStripMenuItem});
+            this.loadMenuItem,
+            this.exportMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "&File";
             // 
-            // loadToolStripMenuItem
+            // loadMenuItem
             // 
-            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
-            this.loadToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
-            this.loadToolStripMenuItem.Text = "&Load";
+            this.loadMenuItem.Name = "loadMenuItem";
+            this.loadMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.loadMenuItem.Text = "&Load";
+            this.loadMenuItem.Click += new System.EventHandler(this.loadMenuItem_Click);
             // 
-            // exportToolStripMenuItem
+            // exportMenuItem
             // 
-            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
-            this.exportToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
-            this.exportToolStripMenuItem.Text = "&Export";
+            this.exportMenuItem.Name = "exportMenuItem";
+            this.exportMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.exportMenuItem.Text = "&Export";
+            this.exportMenuItem.Click += new System.EventHandler(this.exportMenuItem_Click);
             // 
             // tableLayoutPanel1
             // 
@@ -82,7 +84,7 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listView, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -92,35 +94,13 @@
             this.tableLayoutPanel1.TabIndex = 1;
             this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.column1,
-            this.column2});
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(3, 3);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(194, 434);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // column1
-            // 
-            this.column1.Width = 93;
-            // 
-            // column2
-            // 
-            this.column2.Width = 101;
-            // 
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Controls.Add(this.richTextBox1, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.richTextBox2, 0, 2);
-            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.inputTextBox, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.outputTextBox, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.translateButton, 0, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(203, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
@@ -131,35 +111,58 @@
             this.tableLayoutPanel2.Size = new System.Drawing.Size(534, 434);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
-            // richTextBox1
+            // inputTextBox
             // 
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(528, 186);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.inputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputTextBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(3, 3);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(528, 186);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.Text = "";
             // 
-            // richTextBox2
+            // outputTextBox
             // 
-            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox2.Location = new System.Drawing.Point(3, 245);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(528, 186);
-            this.richTextBox2.TabIndex = 1;
-            this.richTextBox2.Text = "";
+            this.outputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputTextBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(3, 245);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(528, 186);
+            this.outputTextBox.TabIndex = 1;
+            this.outputTextBox.Text = "";
             // 
-            // button1
+            // translateButton
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button1.Location = new System.Drawing.Point(229, 205);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Translate!";
-            this.button1.UseVisualStyleBackColor = true;
+            this.translateButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.translateButton.Location = new System.Drawing.Point(229, 205);
+            this.translateButton.Name = "translateButton";
+            this.translateButton.Size = new System.Drawing.Size(75, 23);
+            this.translateButton.TabIndex = 2;
+            this.translateButton.Text = "Translate!";
+            this.translateButton.UseVisualStyleBackColor = true;
+            // 
+            // listView
+            // 
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.GridLines = true;
+            this.listView.Location = new System.Drawing.Point(3, 3);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(194, 434);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Text files (*.txt)|*.txt";
+            this.openFileDialog.Title = "Choose a dictionary file";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "txt";
+            this.saveFileDialog.FileName = "exportedDictionary";
+            this.saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+            this.saveFileDialog.Title = "Save the dictionary file";
             // 
             // Form1
             // 
@@ -186,15 +189,15 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader column1;
-        private System.Windows.Forms.ColumnHeader column2;
+        private System.Windows.Forms.ToolStripMenuItem loadMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportMenuItem;
+        private System.Windows.Forms.ListView listView;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox inputTextBox;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.Button translateButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
 
--- Translator/Form1.cs
+++ Translator/Form1.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -21,5 +24,74 @@ namespace Translator
         {
 
         }
+        
+        private void loadMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            Regex r = new Regex(@"^[a-zA-Z]+ [a-zA-Z]+$");
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            StreamReader sr = new StreamReader(myStream);
+
+                            //Read the file line by line and add the lines to a collection
+                            string line;
+                            bool addedCollmuns = false;
+
+                            //Clear previous data
+                            listView.Columns.Clear();
+                            listView.Items.Clear();
+
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                line.Trim();
+                                if (r.IsMatch(line)) {
+                                    string[] words = line.Split();
+                                    if (addedCollmuns)
+                                    {
+                                        ListViewItem li = new ListViewItem();
+                                        li.Text = words[0];
+                                        li.SubItems.Add(words[1]);
+                                        listView.Items.Add(li);
+                                    }
+                                    else
+                                    {
+                                        listView.Columns.Add(words[0]);
+                                        listView.Columns.Add(words[1]);
+                                        addedCollmuns = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void exportMenuItem_Click(object sender, EventArgs e)
+        {
+            //TODO: Add Exception handeling
+            if (saveFileDialog.ShowDialog() == DialogResult.OK && saveFileDialog.FileName != "" && listView.Columns.Count>=2)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName))
+                {
+                    sw.WriteLine(listView.Columns[0].Text + " " + listView.Columns[1].Text);
+                    for (int i =0; i<listView.Items.Count; i++)
+                    {
+                        sw.WriteLine(listView.Items[i].Text + " " + listView.Items[i].SubItems[1].Text);
+                    }
+                }
+            }
+        }
+        
     }
 }
